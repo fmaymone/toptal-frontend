3d56e90ed019585216d431f2cdaa282b338e6667
FERNANDO MAYMONE DE MELO CARVALHO <49787618@sefaz-ce.gov.br>
2019-02-08 10:59:37 -0300

    MOD stable yarn version to deploy

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -161,7 +161,7 @@ class LandingPage extends Component {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <meta name="apple-mobile-web-app-status-bar-style" content={theme.palette.primary.main} />
           <meta name="msapplication-navbutton-color" content={theme.palette.primary.main} />
-          <title>REACT MOST WANTED</title>
+          <title>Toptal Trips</title>
         </Helmet>
         <AppBar position='static'>
           <Toolbar disableGutters>
@@ -178,18 +178,6 @@ class LandingPage extends Component {
                 <LockIcon />
               </IconButton>
             </Tooltip>
-            <Tooltip id="tooltip-icon2" title="GitHub repository">
-              <IconButton
-                name='github'
-                aria-label='Open Github'
-                color='inherit'
-                href='https://github.com/TarikHuber/react-most-wanted'
-                target='_blank'
-                rel='noopener'
-              >
-                <GitHubIcon />
-              </IconButton>
-            </Tooltip>
           </Toolbar>
         </AppBar>
 
@@ -198,11 +186,7 @@ class LandingPage extends Component {
 
           <div className={classes.hero}>
             <div className={classes.content}>
-              <img
-                src='/rmw.svg'
-                alt='Material-UI Logo'
-                className={classes.logo}
-              />
+             
               <div className={classes.text}>
                 <Typography
                   variant='display2'
@@ -212,7 +196,7 @@ class LandingPage extends Component {
                   gutterBottom
                   className={classes.title}
                 >
-                  {'REACT MOST WANTED'}
+                  {'Toptal Trips'}
                 </Typography>
                 <Typography
                   variant='headline'
@@ -221,7 +205,7 @@ class LandingPage extends Component {
                   gutterBottom
                   className={classes.headline}
                 >
-                  {'React Starter-Kit with all Most Wanted features.'}
+                  {'Best Trips Ever'}
                 </Typography>
                 <Button
                   onClick={() => { history.push('/signin') }}
@@ -231,58 +215,6 @@ class LandingPage extends Component {
                 >
                   {'Get Started'}
                 </Button>
-              </div>
-
-              <div className={classes.cardsContent}>
-                <Card className={classes.card}>
-                  <CardContent>
-                    <Typography variant="headline" component="h2">Installation</Typography>
-                    <br />
-                    <Typography  >{`Just run this script to start:`}</Typography>
-                    <br />
-                    <Typography className={classes.pos} color="textSecondary"> npx create-react-app test-app --scripts-version rmw-react-scripts   </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" onClick={() => {
-                      var win = window.open('https://github.com/TarikHuber/rmw-shell', '_blank')
-                      win.focus();
-                    }} >Learn More</Button>
-                  </CardActions>
-                </Card>
-                <Card className={classes.card}>
-                  <CardContent>
-                    <Typography variant="headline" component="h2">Usage</Typography>
-                    <br />
-                    <Typography  >{`Set your configuration to the App component:`}</Typography>
-                    <br />
-                    <Typography className={classes.pos} color="textSecondary">
-                      {`import App from 'rmw-shell'`}
-                      <br />
-                      {`<App appConfig={{ configureStore, ...config }} />`}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" onClick={() => {
-                      var win = window.open('https://github.com/TarikHuber/react-most-wanted', '_blank')
-                      win.focus();
-                    }} >Learn More</Button>
-                  </CardActions>
-                </Card>
-                <Card className={classes.card}>
-                  <CardContent>
-                    <Typography variant="headline" component="h2">What is this?</Typography>
-                    <Typography noWrap={false} color="textSecondary">
-                      {`This is a OPEN SOURCE demo application that demonstartes the usage of the rmw-shell library 
-                    with react, Material-UI and firebase.  `}
-                      <br />
-                      {` This demo has no purpose to do something as an app. 
-                    It is here just to show how everthing works together. `}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" onClick={() => { history.push('/signin') }} >Get started</Button>
-                  </CardActions>
-                </Card>
               </div>
             </div>
           </div>
