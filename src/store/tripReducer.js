351bf49a26667882808a93a0a169dbcb50c0c7e9
FERNANDO MAYMONE DE MELO CARVALHO <49787618@sefaz-ce.gov.br>
2019-02-14 15:21:32 -0300

    ADD fixed tripReducer

--- src/store/tripReducer.js
+++ src/store/tripReducer.js
@@ -9,64 +9,65 @@ import * as TripActions from '../store/actions/tripActions'
 //The collection Reducer, It handles only the collection
 
 export function TripListReducer(state = [], action) {
-    switch (action.type) {
+	switch (action.type) {
 
-        // The cases ordered in CRUD order.
+		// The cases ordered in CRUD order.
 
-        // Create
-        case TripActions.CREATE_TRIP_SUCCESS: {
-                return [
-                    ...state,
-                    action.trip
-                ];
-        }
-            
-        //Read    
-        case TripActions.GET_TRIPS_SUCCESS: {
-            
-            return action.trips;
+		// Create
+		case TripActions.CREATE_TRIP_SUCCESS: {
+			return [
+				...state,
+				action.trip
+			];
+		}
 
-        }
-        
-        // The following Cases handle the data by mapping it. Mostly because they are related with the modification of a single Data
-        
-        //Update    
-        case TripActions.START_EDITING: {
-            
-            return state.map(s => trip(s, action))
+		//Read    
+		case TripActions.GET_TRIPS_SUCCESS: {
 
-        }
-        case TripActions.CANCEL_EDITING: {
-            
-            return state.map(s => trip(s, action))
+			return action.trips;
 
-        }
-        case TripActions.UPDATE_TRIP: {
+		}
 
-            return state.map(s => trip(s, action))
-            
-        }
-        case TripActions.UPDATE_TRIP_SUCCESS: {
+		// The following Cases handle the data by mapping it. Mostly because they are related with the modification of a single Data
 
-            return state.map(s => trip(s, action))
+		//Update    
+		case TripActions.START_EDITING: {
 
-        }
-        
-        //Delete    
-        case TripActions.DELETE_TRIP: {
+			return state.map(s => trip(s, action))
 
-            return state.map(s => trip(s, action))
+		}
+		case TripActions.CANCEL_EDITING: {
 
-        }
-        case TripActions.DELETE_TRIP_SUCCESS: {
+			return state.map(s => trip(s, action))
 
-            return state.filter(s => trip(s, action))
+		}
+		case TripActions.UPDATE_TRIP: {
 
-        }
-            
-        default:
-            return state
-    }
+			return state.map(s => trip(s, action))
+
+		}
+		case TripActions.UPDATE_TRIP_SUCCESS: {
+
+			return state.map(s => trip(s, action))
+
+		}
+
+		//Delete    
+		case TripActions.DELETE_TRIP: {
+
+			return state.map(s => trip(s, action))
+
+		}
+		case TripActions.DELETE_TRIP_SUCCESS: {
+
+			return state.filter(s => trip(s, action))
+
+		}
+
+
+		default:
+			return state
+	}
 }
 
 
@@ -75,67 +76,72 @@ export function TripListReducer(state = [], action) {
 
 const trip = (state, action) => {
 
-    // If the mapped trip of the previous state matches with the new ID of the action, 
-    // Only then proceed to the Reducer Switch case
+	// If the mapped trip of the previous state matches with the new ID of the action, 
+	// Only then proceed to the Reducer Switch case
 
-    if (state._id != (action._id || action.trip._id)) {
-        return state;
-    }
+	if (state._id != (action._id || action.trip._id)) {
+		return state;
+	}
 
-    switch (action.type) {
+	switch (action.type) {
 
-        // Edit/modifies the individual Trips using ES6 spread operator. The cases are self explanatory.
+		// Edit/modifies the individual Trips using ES6 spread operator. The cases are self explanatory.
 
-        case TripActions.START_EDITING:
-            {
-                return {
-                    ...state,
-                    editing: true
-                }
-            }
+		case TripActions.START_EDITING:
+			{
+				return {
+					...state,
+					editing: true
+				}
+			}
 
-        case TripActions.CANCEL_EDITING:
-            {
-                return {
-                    ...state,
-                    editing: false
-                }
-            }
+		case TripActions.CANCEL_EDITING:
+			{
+				return {
+					...state,
+					editing: false
+				}
+			}
 
-        case TripActions.UPDATE_TRIP:
-            {
-                return {
-                    ...state,
-                    editing: false,
-                    updating: true
-                }
-            }
+		case TripActions.UPDATE_TRIP:
+			{
+				return {
+					...state,
+					editing: false,
+					updating: true
+				}
+			}
 
-        case TripActions.UPDATE_TRIP_SUCCESS:
-            {
-                return {
-                    ...state,
-                    ...action.trip,
-                    updating: false
-                }
-            }
+		case TripActions.UPDATE_TRIP_SUCCESS:
+			{
+				return {
+					...state,
+					...action.trip,
+					updating: false
+				}
+			}
 
-        case TripActions.DELETE_TRIP:
-            {
-                return {
-                    ...state,
-                    deleting: true
-                }
-            }
+		case TripActions.DELETE_TRIP:
+			{
+				return {
+					...state,
+					deleting: true
+				}
+			}
 
-        case TripActions.DELETE_TRIP_SUCCESS:
-            {
-                return false
-            }
+		case TripActions.DELETE_TRIP_SUCCESS:
+			{
+				return false
+			}
 
-        default:
-            {
-                return state;
-            }
-    }
+		case TripActions.DELETE_TRIP_ERROR:
+			{
+				return state;
+			}
+
+		default:
+			{
+				return state;
+			}
+	}
 }
