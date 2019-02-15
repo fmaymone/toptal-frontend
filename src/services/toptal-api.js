5c39d2c092bb36085f6d63e4e6c9405d1d1d1ff5
Fernando Carvalho <gilmour.nando@gmail.com>
2019-02-15 00:25:46 -0300

    Add singleton instance to toptal-api and update tripService test.

--- src/services/toptal-api.js
+++ src/services/toptal-api.js
@@ -1,6 +1,6 @@
 import TripService from './trip'
+import UserService from './user'
 import config from '../config'
 
-let tripService = TripService.create(config.baseURL);
-
-export default tripService;
+export const tripService = TripService.create(config.baseURL);
+export const userService = UserService.create(config.baseURL);
